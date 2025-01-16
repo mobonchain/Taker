f096c1823aaa58cc2bdd76e42d7c422e63d5e0d8
MobGM <81332044+mobonchain@users.noreply.github.com>
2025-01-16 14:20:27 +0700

    Add files via upload

---
+++ config/banner.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const banner = `
+${chalk.white('════════════════════════════════════════════════════════════════')}
+                        ${chalk.bold.cyan('Taker Mining Tool')}
+           ${chalk.yellow('-- Tool shared by GitHub user @rinehaxor --')}
+                  ${chalk.magenta('-- Recover by @mobonchain --')}
+${chalk.white('════════════════════════════════════════════════════════════════')}
+`;
+
+export default banner;
